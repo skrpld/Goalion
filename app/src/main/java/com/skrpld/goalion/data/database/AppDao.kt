6be4db608f16aee72f71dc07050abb00d7ad986d
skrpld <skrpld96@gmail.com>
2025-12-18 23:45:56 +0300

    Update database

--- app/src/main/java/com/skrpld/goalion/data/database/AppDao.kt
+++ app/src/main/java/com/skrpld/goalion/data/database/AppDao.kt
@@ -17,20 +17,8 @@ interface AppDao {
     @Upsert
     suspend fun upsertGoal(goal: Goal): Long
 
-    @Update
-    suspend fun updateGoal(goal: Goal)
-
     @Delete
     suspend fun deleteGoal(goal: Goal)
-
-    @Query("UPDATE goals SET status = :status WHERE id = :goalId")
-    suspend fun updateGoalStatus(goalId: Int, status: TaskStatus)
-
-    @Query("UPDATE goals SET priority = :priority WHERE id = :goalId")
-    suspend fun updateGoalPriority(goalId: Int, priority: TaskPriority)
-
-    @Query("UPDATE goals SET orderIndex = :newOrder WHERE id = :goalId")
-    suspend fun updateGoalOrder(goalId: Int, newOrder: Int)
 
     @Transaction
     @Query("""
@@ -44,18 +32,18 @@ interface AppDao {
     @Upsert
     suspend fun upsertTask(task: Task): Long
 
-    @Update
-    suspend fun updateTask(task: Task)
-
     @Delete
     suspend fun deleteTask(task: Task)
+
+    @Query("UPDATE goals SET status = :status WHERE id = :goalId")
+    suspend fun updateGoalStatus(goalId: Int, status: TaskStatus)
+
+    @Query("UPDATE goals SET priority = :priority WHERE id = :goalId")
+    suspend fun updateGoalPriority(goalId: Int, priority: Int)
 
     @Query("UPDATE tasks SET status = :status WHERE id = :taskId")
     suspend fun updateTaskStatus(taskId: Int, status: TaskStatus)
 
     @Query("UPDATE tasks SET priority = :priority WHERE id = :taskId")
-    suspend fun updateTaskPriority(taskId: Int, priority: TaskPriority)
-
-    @Query("UPDATE tasks SET orderIndex = :newOrder WHERE id = :taskId")
-    suspend fun updateTaskOrder(taskId: Int, newOrder: Int)
+    suspend fun updateTaskPriority(taskId: Int, priority: Int)
 }
